02b8900e46bd53c0044708f72162099f76fdcc44
mohammadGh <mohammad2rmad@gmail.com>
2024-01-12 22:48:21 +0330

    added data base connection and have changed some configurations because some options were pased to mongoose.connect had been deprecated

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useFindAndModify: true,
+      family: 4, // Use IPv4, skip trying IPv6
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
